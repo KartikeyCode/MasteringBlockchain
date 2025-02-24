9d7514566d9f3692eea93e43bc1965516297ee29
KartikeyCode <codekartikey@gmail.com>
2025-02-24 18:31:33 +0530

    test

--- masteringblockchain/app/page.tsx
+++ masteringblockchain/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     router.push("/private")
   }
   return (
-    <div className="w-full min-h-screen flex items-center justify-center">
+    <div className="w-full min-h-screen flex items-center justify-center ">
       <Navbar />
       <button onClick={handleClick} className="px-10 py-5 rounded-xl hover:scale-105 transition-all bg-[#034DA1] text-white text-3xl border-2 border-black"> View resources </button>
     </div>
